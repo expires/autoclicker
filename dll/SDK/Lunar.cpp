609b143be5d4afddc59765e3723243de8305d061
expires <73852745+expires@users.noreply.github.com.>
2026-03-13 19:40:56 +0000

    Add debug

--- dll/SDK/Lunar.cpp
+++ dll/SDK/Lunar.cpp
@@ -3,8 +3,13 @@
 void Lunar::GetLoadedClasses()
 {
     jvmtiEnv *jvmti;
-    if (vm->GetEnv((void **)&jvmti, JVMTI_VERSION_1_2) != JNI_OK)
+    jint jvmtiResult = vm->GetEnv((void **)&jvmti, JVMTI_VERSION_1_2);
+    if (jvmtiResult != JNI_OK)
+    {
+        printf("[AC] Failed to get JVMTI env (result=%d)\n", jvmtiResult);
         return;
+    }
+    printf("[AC] Got JVMTI env\n");
 
     jclass lang = env->FindClass("java/lang/Class");
     jmethodID getName = env->GetMethodID(lang, "getName", "()Ljava/lang/String;");
@@ -13,12 +18,26 @@ void Lunar::GetLoadedClasses()
     jint amount;
 
     jvmti->GetLoadedClasses(&amount, &classesPtr);
+    printf("[AC] Total loaded classes: %d\n", amount);
+
     for (int i = 0; i < amount; i++)
     {
         jstring name = (jstring)env->CallObjectMethod(classesPtr[i], getName);
         const char *className = env->GetStringUTFChars(name, 0);
         classes.emplace(std::make_pair((std::string)className, classesPtr[i]));
         env->ReleaseStringUTFChars(name, className);
+    }
+
+    printf("[AC] Classes stored in map: %zu\n", classes.size());
+    // Print first 10 minecraft-related class names to verify naming
+    int printed = 0;
+    for (const auto& [name, cls] : classes)
+    {
+        if (name.find("minecraft") != std::string::npos && printed < 10)
+        {
+            printf("[AC] Sample class: %s\n", name.c_str());
+            printed++;
+        }
     }
 }
 
