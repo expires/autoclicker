42cc015e34284f038c8c0fc333e3851567e43b00
expires <73852745+expires@users.noreply.github.com.>
2026-03-14 17:42:08 +0000

    Fix issues

--- dll/main.cpp
+++ dll/main.cpp
@@ -13,6 +13,7 @@ BOOL APIENTRY DllMain(const HINSTANCE instance, const DWORD reason, LPVOID reser
     if (reason == DLL_PROCESS_ATTACH)
     {
         DisableThreadLibraryCalls(instance);
+        McBotLogInit();
         McBotLog("DLL loaded");
         Overlay::Init();
         CreateThread(nullptr, 0, AutoclickerModule::init, instance, 0, nullptr);
